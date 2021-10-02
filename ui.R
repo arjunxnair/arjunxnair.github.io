5cf043e70f38fcbdd2ef6dc999877c015a613c15
arjunxnair <58760579+arjunxnair@users.noreply.github.com>
2021-10-02 16:00:48 +1000

    updated

--- ui.R
+++ ui.R
@@ -30,16 +30,9 @@ ui <- dashboardPage(
     tabItems(
       # First tab content
       tabItem(tabName = "dashboard",
-              fluidRow(
-                box(plotOutput("plot1", height = 250)),
-                
-                box(
-                  title = "Controls",
-                  sliderInput("slider", "Number of observations:", 1, 100, 50)
-                )
-              )
+              h2("dashboard tab content")
       ),
-      
+
       # Second tab content
       tabItem(tabName = "widgets",
               h2("Widgets tab content")
@@ -47,3 +40,46 @@ ui <- dashboardPage(
     )
   )
 )
+
+
+# 
+# ui <- fluidPage(
+#   sidebarLayout(
+#     sidebarPanel(
+#       width = 3,
+#       h2("My Static website"),
+#       h3("Internal link example"),
+#       a(h4("Birds"),
+#         href = "#birds"),
+#       hr(),
+#       
+#       h3("External link example"),
+#       a(h4("Wikipedia"),
+#         href = "https://en.wikipedia.org/wiki/Main_Page",
+#         target = "blank")
+#     ),
+#     
+#     mainPanel(
+#       width = 9,
+#       
+#       h2("Link to a saved sample.html"),
+#       p("The url is https://johndoe.github.io/samples/sample.html"),
+#       a(h3("Lovely Birds"),
+#         href = "https://johndoe.github.io/samples/sample.html",
+#         target = "blank"),
+#       hr(),
+#       
+#       h2("Text and image example", id = "birds"),
+#       fluidRow(
+#         column(
+#           7,
+#           includeMarkdown("markdown/birds.Rmd")
+#         ),
+#         column(
+#           5,
+#           img(src = "birds.png", width = "100%")
+#         )
+#       ),
+#     )
+#   )
+# )
