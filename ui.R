9233a37583bc37a59a9a951baafee6d479c5df19
arjunxnair <58760579+arjunxnair@users.noreply.github.com>
2021-10-02 17:02:53 +1000

    updated

--- ui.R
+++ ui.R
@@ -17,9 +17,16 @@ dashHeader<- dashboardHeader(
 )
 
 dashSidebar<- dashboardSidebar(
-  width = 250,
-  tags$a(href='https://arjunxnair.github.io/',
-         tags$img(src='arjunxnair.png',height='250',width='250')
+  width = 251,
+  # tags$a(href='https://arjunxnair.github.io/',
+  #        tags$img(src='arjunxnair.png',height='250',width='250')
+  # 
+  # )
+  tags$div(
+    HTML('<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
+         <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="arjunxnair" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://au.linkedin.com/in/arjunxnair?trk=profile-badge"></a></div>
+              
+         ')
   )
 )
 
